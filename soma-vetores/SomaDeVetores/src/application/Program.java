66d6902f4b9d58d3ab8ef768a7aad35881f58460
matt-henri <mathmarkub@gmail.com>
2023-06-01 19:39:46 -0300

    atualizado

---
+++ soma-vetores/SomaDeVetores/src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		double soma, media;
+		
+		System.out.print("Quantos números vai Digitar: ");
+		n = sc.nextInt();
+		
+		double[] vetor = new double[n];
+		
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um numero: ");
+			vetor[i] = sc.nextDouble();
+		}
+		
+		soma = 0;
+		for (int i = 0; i < n; i++) {
+			soma = soma + vetor[i];
+		}
+		
+		media = soma / n;
+		
+		System.out.print("Valores = ");
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%.2f  -  ", vetor[i]);
+		}
+		
+		
+		System.out.printf("\nSoma = %.2f\n", soma);
+		System.out.printf("Medida = %.2f", media);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
