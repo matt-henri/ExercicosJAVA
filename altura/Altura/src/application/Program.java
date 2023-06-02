687632dbfcfde92df7b1573586a77c1706884b8e
matt-henri <mathmarkub@gmail.com>
2023-06-02 13:02:35 -0300

    exerAltura

---
+++ altura/Altura/src/application/Program.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n, nmenores;
+		double alturaTotal, alturaMedia, percentualMenores;
+		
+		System.out.print("Quantas pessoas serão digitadas? ");
+		n = sc.nextInt();
+		
+		String[] nomes = new String[n];
+		int[] idades = new int[n];
+		double[] alturas = new double[n];
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Dados da pessoa: \n", i + 1);
+			System.out.print("Nome: ");
+			nomes[i] = sc.next();
+			System.out.print("Idade: ");
+			idades[i] = sc.nextInt();
+			System.out.print("Altura: ");
+			alturas[i] = sc.nextDouble();
+		}
+		
+		nmenores = 0;
+		alturaTotal = 0;
+		for (int i = 0; i < n; i++) {
+			if (idades[i] < 18) {
+				nmenores++;
+			}
+			alturaTotal = alturaTotal + alturas[i];
+		}
+		
+		alturaMedia = alturaTotal / n;
+		percentualMenores = ((double)nmenores / n) * 100.0;
+		
+		
+		System.out.printf("\nAltura media = %.2f\n", alturaMedia);
+		System.out.printf("Pessoas com menos de 18 anos: %.1f%%\n", percentualMenores);
+		
+		for (int i = 0; i < n; i++) {
+			if (idades[i] < 18) {
+				System.out.printf("%s\n", nomes[i]);
+			}
+		}
+		
+		
+		
+		
+		sc.close();
+		
+		
+	}
+
+}
